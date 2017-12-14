b2803437bff937c361f2a03ea343be2e85667929
Geekduan <geekdmm@163.com>
2017-12-14 22:02:51 +0800

    add note

--- scene/begin/begin-scene.js
+++ scene/begin/begin-scene.js
@@ -1,6 +1,7 @@
 class TitleLabel {
-    constructor(game) {
+    constructor(game, text) {
         this.game = game
+        this.text = text
     }
 
     update() {
@@ -9,7 +10,7 @@ class TitleLabel {
     draw() {
         this.game.context.fillStyle = 'red';
         this.game.context.font = "20px Georgia"
-        this.game.context.fillText('按 r 开始游戏, 按 v 关卡编辑', 70, 200)
+        this.game.context.fillText(this.text, 70, 200)
     }
 }
 
@@ -17,7 +18,7 @@ class SceneBegin extends BaseScene {
     constructor(game) {
         super(game)
         // event
-        var l = new TitleLabel(game)
+        var l = new TitleLabel(game, '粒子系统')
         this.addElemet(l)
     }
 }
