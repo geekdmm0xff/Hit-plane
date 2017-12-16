1a14e7833e86f3ef661a2e5863cf2c4bb96b0df4
Geekduan <geekdmm@163.com>
2017-12-16 23:47:05 +0800

    add ps file

--- scene/begin/begin-scene.js
+++ scene/begin/begin-scene.js
@@ -12,74 +12,6 @@ class TitleLabel {
     }
 }
 
-class Particle extends GuaImage {
-    constructor(game, x, y) {
-        super('fire', game)
-        this.init(x, y)
-        this.setup()
-    }
-
-    init(x, y) {
-        this.x = x
-        this.y = y
-    }
-
-    setup() {
-        this.w *= 0.2
-        this.h *= 0.2
-        this.vx = random(-5, 5)
-        this.vy = random(-5, 5)
-
-        this.life = 5
-    }
-
-    update(){
-        this.life--
-
-        this.x += this.vx
-        this.y += this.vy
-
-        var factor = 0.01
-        this.x += this.x * factor
-        this.y += this.x * factor
-    }
-}
-
-class ParticleSystem {
-    constructor(game) {
-        this.game = game
-        this.particles = []
-        this.numberOfParticels = 10
-        this.duration = 50
-    }
-
-    addParticle() {
-        var p = new Particle(this.game, this.x, this.y)
-        this.particles.push(p)
-    }
-
-    update() {
-        this.duration--
-        if (this.particles.length < this.numberOfParticels) {
-            this.addParticle()
-        }
-        for (var p of this.particles) {
-            p.update()
-        }
-        this.particles = this.particles.filter(p => p.life > 0)
-    }
-
-    draw() {
-        if (this.duration <= 0) {
-            this.scene.removeElement(this)
-            return
-        }
-        for (var p of this.particles) {
-            p.draw()
-        }
-    }
-}
-
 class SceneBegin extends BaseScene {
     constructor(game) {
         super(game)
@@ -88,8 +20,6 @@ class SceneBegin extends BaseScene {
         this.addElemet(l)
 
         var ps = new ParticleSystem(game)
-        ps.x = 100
-        ps.y = 100
         this.addElemet(ps)
     }
 }
